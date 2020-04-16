b93831df74421614b976858fe8bae3405e169415
Sangeeta Sharma <1998sangeetasharma@gmail.com>
2020-04-16 23:49:53 +0530

    changed baseUrl from local to heroku

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthenticationService {
-  baseUrl: string = "http://localhost:5000";
+  baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class ImagesService {
-  baseUrl: string = "http://localhost:5000";
+  baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
